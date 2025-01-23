c0e757dc125451a989ac59ffced7966810358fd9
Aigio Liu <lysoft.pw@foxmail.com>
2025-01-23 14:30:50 +0800

    🔖 3.0.0-rc.14

--- src/AssemblyInfo.Constants.cs
+++ src/AssemblyInfo.Constants.cs
@@ -21,7 +21,7 @@ public static partial class AssemblyInfo
     /// <summary>
     /// RC 版本号，范围 0~9，只增不减
     /// </summary>
-    const string ver_for_rc = "13";
+    const string ver_for_rc = "14";
 
     public const string FileVersion = $"{Version2}.{ver_for_rc}2{ver_for_preview}.0";
 
